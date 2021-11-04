2d6a030cb1a283997681255e4d69fb9baa1efb08
Percy <alex1998u@hotmail.com>
2021-11-04 14:04:33 -0500

    fourth commit

---
+++ src/analizador/Tokens.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package analizador;
+
+/**
+ *
+ * @author alex1
+ */
+public enum Tokens {
+    
+    ent, 
+    re,
+    cad,
+    car,
+    Principal,
+    AbreParen,
+    CierraParen,
+    AbreLlave,
+    CierraLlave,
+    Error
+    
+}
